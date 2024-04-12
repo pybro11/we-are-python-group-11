1965f224295ac4a8c068f221d8f87bd2cb48a1fa
Lucy <164493549+Lucypothesis@users.noreply.github.com>
2024-04-12 19:35:31 +0900

    Update main.py

--- streamlit/main.py
+++ streamlit/main.py
@@ -2,15 +2,6 @@ import streamlit as st
 
 st.title('트렌드를 분석하는 11조입니다.💫')
 
-import audio
-if 'recording_started' not in st.session_state:
-    st.session_state.recording_started = False
-
-if st.button("음성 녹음 시작", key='start_rec'):
-    st.session_state.recording_started = True
-
-if st.session_state.recording_started:
-    audio.audiorec_demo_app()
 
 import column12
 
@@ -20,3 +11,13 @@ import ploty
 
 import webinput
 
+import audio
+
+if 'recording_started' not in st.session_state:
+    st.session_state.recording_started = False
+
+if st.button("음성 녹음 시작", key='start_rec'):
+    st.session_state.recording_started = True
+
+if st.session_state.recording_started:
+    audio.audiorec_demo_app()
