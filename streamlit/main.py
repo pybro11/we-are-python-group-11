f8c730228e06dbf61b3fd5cffe920ad4841b7b52
Lucy <164493549+Lucypothesis@users.noreply.github.com>
2024-04-12 21:39:41 +0900

    Update main.py

--- streamlit/main.py
+++ streamlit/main.py
@@ -11,13 +11,19 @@ import ploty
 
 import webinput
 
-import audio
+from whisper_stt import whisper_stt
 
-if 'recording_started' not in st.session_state:
-    st.session_state.recording_started = False
+text = whisper_stt(openai_api_key="<your_api_key>", language = 'en')  
+# If you don't pass an API key, the function will attempt to retrieve it as an environment variable : 'OPENAI_API_KEY'.
+if text:
+    st.write(text)
 
-if st.button("음성 녹음 시작", key='start_rec'):
-    st.session_state.recording_started = True
+# import audio
+# if 'recording_started' not in st.session_state:
+#     st.session_state.recording_started = False
 
-if st.session_state.recording_started:
-    audio.audiorec_demo_app()
+# if st.button("음성 녹음 시작", key='start_rec'):
+#     st.session_state.recording_started = True
+
+# if st.session_state.recording_started:
+#     audio.audiorec_demo_app()
