af5f246faa8eb7c97716cb3104a88cae04baf1d2
delightflow <contact@patentwith.com>
2024-04-12 17:40:21 +0900

    streamlit toml issue
    Streamlit deploy 전에 secrets에 api json 넣어야 합니다.

--- streamlit/audio.py
+++ streamlit/audio.py
@@ -1,8 +1,27 @@
 from st_audiorec import st_audiorec
 import streamlit as st 
-
+import json
 from google.cloud import speech
 
+from google.oauth2 import service_account
+
+# Load the JSON secret
+service_account_info = {
+    "type": st.secrets["type"],
+    "project_id": st.secrets["project_id"],
+    "private_key_id": st.secrets["private_key_id"],
+    "private_key": st.secrets["private_key"],
+    "client_email": st.secrets["client_email"],
+    "client_id": st.secrets["client_id"],
+    "auth_uri": st.secrets["auth_uri"],
+    "token_uri": st.secrets["token_uri"],
+    "auth_provider_x509_cert_url": st.secrets["auth_provider_x509_cert_url"],
+    "client_x509_cert_url": st.secrets["client_x509_cert_url"]
+}
+
+# Create the SpeechClient with the loaded credentials
+credentials = service_account.Credentials.from_service_account_info(service_account_info)
+client = speech.SpeechClient(credentials=credentials)
 
 # DESIGN implement changes to the standard streamlit UI/UX
 # --> optional, not relevant for the functionality of the component!
@@ -19,15 +38,15 @@ st.markdown('''<style>.css-v37k9u a {color: #ff4c4b;}</style>''',
 st.markdown('''<style>.css-nlntq9 a {color: #ff4c4b;}</style>''',
             unsafe_allow_html=True)  # lightmode
 
-# Google Cloud 클라이언트 설정
-client = speech.SpeechClient()
+
 
 def transcribe_google(audio_bytes):
     """Google Cloud Speech-to-Text를 사용하여 오디오 바이트를 텍스트로 변환합니다."""
     audio = speech.RecognitionAudio(content=audio_bytes)
     config = speech.RecognitionConfig(
         encoding=speech.RecognitionConfig.AudioEncoding.LINEAR16,
-        sample_rate_hertz=44100,
+        sample_rate_hertz=48000,
+        audio_channel_count=2,
         language_code="ko-KR"  # 여기서 언어 설정을 조정할 수 있습니다.
     )
 
--- streamlit/main.py
+++ streamlit/main.py
@@ -5,8 +5,7 @@ st.title('트렌드를 분석하는 11조입니다.💫')
 from st_audiorec import st_audiorec
 
 from audio import audiorec_demo_app
-if st.button('음성 녹음 시작'):
-    audiorec_demo_app()  # 버튼 클릭 시 audiorec_demo_app 함수를 호출합니다.
+audiorec_demo_app()  # 버튼 클릭 시 audiorec_demo_app 함수를 호출합니다.
 
 import column12
 
