b635f73cf9096ca85bf6496c880cb91d04d2cc7f
Lucy <164493549+Lucypothesis@users.noreply.github.com>
2024-04-12 18:39:23 +0900

    Update main.py

--- streamlit/main.py
+++ streamlit/main.py
@@ -2,10 +2,9 @@ import streamlit as st
 
 st.title('트렌드를 분석하는 11조입니다.💫')
 
-from st_audiorec import st_audiorec
-
 from audio import audiorec_demo_app
 audiorec_demo_app()  # 버튼 클릭 시 audiorec_demo_app 함수를 호출합니다.
+import audio
 
 import column12
 
