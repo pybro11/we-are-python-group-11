57403ca010d3d90d8ad8b01a75b0a28372b61d3c
Lucy <164493549+Lucypothesis@users.noreply.github.com>
2024-04-13 13:46:05 +0900

    Update main.py

--- main.py
+++ main.py
@@ -41,9 +41,9 @@ def main():
     
     def STT(audio):
         filename='input.mp3'
-        audio.export(filename, format='mp3')  # AudioSegment를 mp3 파일로 저장
-        audio_file = AudioSegment.from_file(filename)
-        # wav_file.close()
+        wav_file = open(filename, "wb")
+        wav_file.write(audio.tobytes())
+        wav_file.close()
     
         # 음원 파일 열기
         audio_file = open(filename, "rb")
@@ -63,14 +63,21 @@ def main():
         # gTTS 를 활용하여 음성 파일 생성
         filename = "output.mp3"
         tts = gTTS(text=response,lang="ko")
-        tts.save(filename, format='mp3')
+        tts.save(filename)
     
         # 음원 파일 자동 재생
-        audio = AudioSegment.from_file(filename)
-        play(audio)  # 재생
+        with open(filename, "rb") as f:
+            data = f.read()
+            b64 = base64.b64encode(data).decode()
+            md = f"""
+                <audio autoplay="True">
+                <source src="data:audio/mp3;base64,{b64}" type="audio/mp3">
+                </audio>
+                """
+            st.markdown(md,unsafe_allow_html=True,)
         # 파일 삭제
         os.remove(filename)
-
+    
     col1, col2 =  st.columns([3,5])
     with col1:
         st.subheader("어떤 것이 궁금한가요?")
